a4e28a1ae863ef318a76ec6ed21c93b36466c90e
zimzaza4 <1875162740@qq.com>
2023-01-27 14:53:41 +0800

    use pre login

--- src/main/java/me/zimzaza4/forceplayerlink/ForcePlayerLink.java
+++ src/main/java/me/zimzaza4/forceplayerlink/ForcePlayerLink.java
@@ -1,8 +1,12 @@
 package me.zimzaza4.forceplayerlink;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -17,6 +21,7 @@ import java.util.UUID;
 public final class ForcePlayerLink extends JavaPlugin implements Listener {
 
     private static ForcePlayerLink instance;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -29,38 +34,37 @@ public final class ForcePlayerLink extends JavaPlugin implements Listener {
         // Plugin shutdown logic
     }
 
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        new BukkitRunnable() {
-            @Override
-            public void run() {
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerJoin(AsyncPlayerPreLoginEvent event) {
 
-                PlayerLink playerLink = InstanceHolder.getPlayerLink();
+        PlayerLink playerLink = InstanceHolder.getPlayerLink();
 
-                FloodgateApi floodgateApi = FloodgateApi.getInstance();
-                if (floodgateApi.isFloodgatePlayer(event.getPlayer().getUniqueId())) {
-                    getLogger().info("Bedrock Player");
-                    playerLink.isLinkedPlayer(event.getPlayer().getUniqueId()).whenComplete((isLinked, t) -> {
-                        if (isLinked) {
-                            return;
-                        }
-                        getLogger().info("Linked");
-                        FloodgatePlayer floodgatePlayer = floodgateApi.getPlayer(event.getPlayer().getUniqueId());
-                        String name = floodgatePlayer.getUsername();
-                        if (getConfig().getBoolean("replace-space")) {
-                            name = name.replace(" ", "_");
-                        }
-                        playerLink.linkPlayer(event.getPlayer().getUniqueId(), createOfflinePlayerUuid(name), name);
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                event.getPlayer().kickPlayer("正在为您LinkAccount, 请重新进入");
-                            }
-                        }.runTask(instance);
-                    });
+        FloodgateApi floodgateApi = FloodgateApi.getInstance();
+        if (floodgateApi.isFloodgatePlayer(event.getUniqueId())) {
+            getLogger().info("Bedrock Player");
+            playerLink.isLinkedPlayer(event.getUniqueId()).whenComplete((isLinked, t) -> {
+                if (isLinked) {
+                    return;
                 }
-            }
-        }.runTaskLater(this, 2);
+                getLogger().info("Linked");
+                FloodgatePlayer floodgatePlayer = floodgateApi.getPlayer(event.getUniqueId());
+                String name = floodgatePlayer.getUsername();
+                if (getConfig().getBoolean("replace-space")) {
+                    name = name.replace(" ", "_");
+                }
+                playerLink.linkPlayer(event.getUniqueId(), createOfflinePlayerUuid(name), name);
+
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        Player player = Bukkit.getPlayer(event.getUniqueId());
+                        if (player != null) {
+                            player.kickPlayer("正在为您LinkAccount, 请重新进入");
+                        }
+                    }
+                }.runTaskLater(this, 5);
+            });
+        }
     }
 
 
